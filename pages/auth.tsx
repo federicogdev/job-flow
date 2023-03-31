84d57ee4191fd1066f50c8ef5adfe04013fa2dc4
Federico Giorgino <grgfdrc@gmail.com>
2023-04-01 01:01:57 +0200

    Eslint fix

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -83,7 +83,7 @@ const Auth = (props: Props) => {
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
-          <img src="/images/logo.png" className="h-12" alt="Logo" />
+          {/* <img src="/images/logo.png" className="h-12" alt="Logo" /> */}
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
@@ -126,7 +126,7 @@ const Auth = (props: Props) => {
 
             <p className="text-neutral-500 mt-12">
               {variant === "login"
-                ? "First time using Netflix?"
+                ? "First time around?"
                 : "Already have an account?"}
               <span
                 onClick={toggleVariant}
