f61307b991cbc99be814dc8e4cd84559bad0ca13
Federico Giorgino <grgfdrc@gmail.com>
2023-04-01 13:19:54 +0200

    Test Form to test functionality

--- pages/api/jobs/index.tsx
+++ pages/api/jobs/index.tsx
@@ -1,4 +1,7 @@
+import serverAuth from "@/libs/serverAuth";
 import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/libs/prisma";
+import { JobStatus, JobType } from "@prisma/client";
 
 export default async function handler(
   req: NextApiRequest,
@@ -8,9 +11,30 @@ export default async function handler(
     return res.status(405).end();
   }
   try {
+    const { currentUser } = await serverAuth(req);
+
     if (req.method === "POST") {
+      const { company, location, position, status, type } = req.body;
+      const job = await prisma.job.create({
+        data: {
+          company,
+          location,
+          position,
+          status: status as JobStatus,
+          type: type as JobType,
+          userId: currentUser.id,
+        },
+      });
+
+      return res.status(200).json(job);
     }
+
     if (req.method === "GET") {
+      const jobs = await prisma.job.findMany({
+        where: { userId: currentUser.id },
+      });
+
+      return res.status(200).json(jobs);
     }
   } catch (error) {
     return res.status(400).json({ error: `Something went wrong: ${error}` });
